7685120437a9b1c1681fea7d0913d7e3f2663bad
eukerolyne <ribeirodias2003@gmail.com>
2024-12-09 18:40:21 -0300

    substituir link de remoção por botão e otimizar a função de remoção de repositórios

--- src/components/ItemRepo/index.js
+++ src/components/ItemRepo/index.js
@@ -13,7 +13,8 @@ function ItemRepo({repo, handleRemoveRepo}) {
         <h3>{repo.name}</h3>
         <p>{repo.full_name}</p>
         <a href={repo.html_url} rel="noreferrer" target="_blank">Ver repositório</a><br />
-        <a href="#"  rel="noreferrer" className="remover">Remover</a>
+        <button onClick={() => handleRemoveRepo(repo.id)}>Remover</button>
+        
         <hr />
     </ItemContainer>
   )
